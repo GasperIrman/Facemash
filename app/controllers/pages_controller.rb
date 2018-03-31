b56993f32fad0bb67d832bf023faba07b6d78b49
Gasper <gasper.irman@gmail.com>
2018-03-31 20:41:19 +0200

    Added login, signup

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+	before_action :require_login
+	def show
+	end
+
+	def ratings	
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 	 root to: 'pages#mash'
+
+  	get 'Ratings' => 'pages#ratings', as: 'ratings'
+ 	get 'Mash' => 'pages#mash', as: 'mash'
 end
